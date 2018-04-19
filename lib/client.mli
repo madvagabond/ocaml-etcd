2c603c02c462aa7c277fb464999ef7177b454ef5
sam <nullvoidnil@mail.com>
2018-04-19 11:04:03 -0400

    wrote implementation for key api

--- lib/client.mli
+++ lib/client.mli
@@ -3,16 +3,17 @@ open Protocol
 exception Neither
             
 type result = (Response.t, Error.t) Result.result
+type key = string list
                                     
-val put: string -> string -> ?ttl:int -> () -> result Lwt.t 
-val get: string -> string -> result Lwt.t
+val put: string -> key -> ?ttl:int -> string -> result Lwt.t 
+val get: string -> key -> result Lwt.t
                                     
-val delete: string -> string -> result Lwt.t
-val create_dir: string -> string -> ?ttl:int -> unit -> result Lwt.t
+val delete: string -> key -> result Lwt.t
+val create_dir: string -> key -> ?ttl:int -> unit -> result Lwt.t
 
-val refresh: string -> string -> result Lwt.t                                        
-val list: string -> string -> ?r:bool -> unit -> result Lwt.t
-val rm_dir: string -> string -> result Lwt.t                                   
+val refresh: string -> key -> int -> result Lwt.t                                        
+val list: string -> key -> ?r:bool -> unit -> result Lwt.t
+val rm_dir: string -> key -> result Lwt.t                                   
                                    
-val watch: string -> string -> (fun result -> 'a) -> ?times:int -> unit -> 'a Lwt.t
-val refresh: string -> string -> result Lwt.t  
+val watch: string -> key -> (Response.t -> 'a) -> ?times:int -> unit -> 'a Lwt.t
+val refresh: string -> key -> result Lwt.t  
